83f93fcac1c3a15e20b75c84f326c67e091d9a05
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-04-29 19:18:41 +0200

    set up for routing and making sure all router paths work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,13 @@ import { OrderFormComponent } from './order-form/order-form.component';
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 import { AdminComponent } from './admin/admin.component';
 import { CartItemsComponent } from './cart-items/cart-items.component';
+
+const appRoutes = [
+  /*{path: 'details/:id', component: DetailsComponent},*/
+  {path: 'home', component: HomeComponent}, 
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  /*{path: '**', component: NotfoundComponent}*/
+];
 
 @NgModule({
   declarations: [
@@ -24,7 +32,11 @@ import { CartItemsComponent } from './cart-items/cart-items.component';
     CartItemsComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
