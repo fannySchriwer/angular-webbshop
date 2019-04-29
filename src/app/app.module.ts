d38a6313e75910c2c5cca192019b92ed11c884fc
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-04-29 17:52:01 +0200

    added all basic components and interface folder plus two interface files for product and order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './home/products/products.component';
+import { PrintProductComponent } from './home/products/print-product/print-product.component';
+import { DetailsComponent } from './details/details.component';
+import { OrderFormComponent } from './order-form/order-form.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { AdminComponent } from './admin/admin.component';
+import { CartItemsComponent } from './cart-items/cart-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    PrintProductComponent,
+    DetailsComponent,
+    OrderFormComponent,
+    OrderConfirmComponent,
+    AdminComponent,
+    CartItemsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/home/products/print-product/print-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-print-product',
+  templateUrl: './print-product.component.html',
+  styleUrls: ['./print-product.component.css']
+})
+export class PrintProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
